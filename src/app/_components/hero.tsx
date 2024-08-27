000e5a656556df483fa6375a376cb6e9b8783a26
Dmitry Yefymov <a1111@MacBook-Pro.local>
2024-08-27 19:40:32 +0300

    first commit

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,27 @@
+import styles from "./hero.module.css";
+import Image from "next/image";
+import heroImage from "../../../public/images/hero1.jpeg";
+
+const Hero = () => {
+    return (
+        <div className={styles.hero}>
+            <h2 className={styles.hero_title}>
+                <div className={styles.hero_big_text}>Front-end</div>
+                <div className={styles.hero_big_text}>Developer</div>
+            </h2>
+
+            <div className={styles.hero_wrapper}>
+                <div className={styles.image_container}>
+                    <Image src={heroImage} alt="" />
+                </div>
+
+                <div className={styles.hero_name}>
+                    <div className={styles.hero_big_text}>Dmytro</div>
+                    <div className={styles.hero_big_text}>Yefymov</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
