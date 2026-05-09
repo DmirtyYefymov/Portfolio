3d6ca6d412de60bd7051bf72e685fd9da85623c7
DmytroYefymov <efimov.dmitry.33@gmail.com>
2026-05-09 13:37:20 +0300

    fix: add type declaration for .jpeg files in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,9 @@
+declare module "*.jpeg" {
+    import type { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
 declare module "*.css" {
     const content: { [className: string]: string };
     export default content;
